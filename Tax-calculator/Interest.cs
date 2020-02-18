01d01995cd2119f4860b0d659664d97c28765ffe
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-02-18 16:01:28 +0200

    add a class

---
+++ Tax-calculator/Interest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tax_calculator
+{
+    class Interest
+    {
+        public double DikeopratikosTokos { get; set; }
+        public double TokosYperimerias { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+        public Interest(DateTime startDate, DateTime endDate, double dikeopratikosTokos, double tokosYperimerias)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            DikeopratikosTokos = dikeopratikosTokos;
+            TokosYperimerias = tokosYperimerias;
+        }
+
+    }
+}
