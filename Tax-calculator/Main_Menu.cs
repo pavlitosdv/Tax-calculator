19eac693904f4e924a8dcccf4a403cc3bcd2dcce
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-02-20 16:37:06 +0200

    Calculation functions

---
+++ Tax-calculator/Main_Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tax_calculator
+{
+    class Main_Menu
+    {
+
+
+
+        private double Calculate_Dikeopratikos_Tokos(double capital, double DikeopratikosTokos, TimeSpan days)
+        {
+            return (capital * DikeopratikosTokos * (((double)days.TotalDays) / (double)360));
+        }
+
+        private double Calculate_Tokos_Yperimerias(double capital, double TokosYperimerias, TimeSpan days)
+        {
+            return (capital * TokosYperimerias * (((double)days.TotalDays) / (double)360));
+        }
+
+    }
+}
