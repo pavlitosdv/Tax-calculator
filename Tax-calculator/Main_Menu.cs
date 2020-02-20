811affebc3bb724b1a44988410d7592edf9e9833
pavlitosdv <pavlos.papadopoulos88@gmail.com>
2020-02-20 16:54:30 +0200

    print to console functions

--- Tax-calculator/Main_Menu.cs
+++ Tax-calculator/Main_Menu.cs
@@ -21,5 +21,24 @@ namespace Tax_calculator
             return (capital * TokosYperimerias * (((double)days.TotalDays) / (double)360));
         }
 
+
+        private void PrintEachPeriodDetails(DateTime startDate, DateTime endDate, TimeSpan days, Interest dtss, double dikeopratikos, double yperimerias)
+        {
+            Console.WriteLine("Start date: {0, -6} | End date: {1, -5} | days: {2, -3}", startDate.ToString("MMM d, yyyy"), endDate.ToString("MMM d, yyyy"), days.Days);
+            Console.WriteLine("Dikeopratikos rate: {0, -5} | Interest: {1, -5:C2}", dtss.DikeopratikosTokos, dikeopratikos);
+            Console.WriteLine("Yperimerias rate: {0, -5} | Interest: {1, -5:C2}", dtss.TokosYperimerias, yperimerias);
+            Console.WriteLine();
+        }
+
+        private void PrintTotalAmount(double capital, double local_store1, double local_store2)
+        {
+            Console.WriteLine("Initial Capital: {0, -6}", capital);
+            Console.WriteLine("Total Dikeopratikos Rate: {0, -6:C2}", local_store1);
+            Console.WriteLine("Total Yperimerias Rate: {0, -6:C2}", local_store2);
+            Console.WriteLine("Total Payment: {0, -6:C2}", local_store1 + local_store2 + capital);
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine();
+        }
     }
 }
